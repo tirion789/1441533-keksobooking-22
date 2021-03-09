f74d27ee68d656fcadfca8c24498b6d9215e0f6d
tirion <tirionchen@mail.ru>
2021-03-09 18:06:57 +0300

    Перламутровые пуговицы (часть 2)

--- js/main.js
+++ js/main.js
@@ -6,17 +6,19 @@ import './form.js';
 import './map.js';
 import './filters.js';
 import './popups.js';
-import { createOffers,   removeMarkers } from './map.js';
+import { createOffers } from './map.js';
 import { getData } from './server-api.js';
-import { resetMapCoordinate } from './map.js';
-import { setFiltersBottonReset, setFiltersChange} from './filters.js';
+import { resetMapCoordinate, removeMarkers } from './map.js';
+import { setFiltersBottonReset, setFiltersChange} from './filters.js'
 
+const RERENDER_DELAY = 500;
 
 getData((offers) => {
   createOffers(offers);
-  setFiltersChange(() => {
-    removeMarkers(offers);
-    createOffers(offers);})
+  setFiltersChange(_.debounce(
+    () => (removeMarkers(offers),
+    createOffers(offers)),
+    RERENDER_DELAY));
   setFiltersBottonReset(() => createOffers(offers));
 });
 
